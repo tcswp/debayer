87a50ecf07b2f0859722a79d84e8d5e3661fd124
Steven Hastings <borracha630@yahoo.com>
2022-04-03 18:45:28 -0400

    test with new image

--- test.m
+++ test.m
@@ -1,4 +1,13 @@
-I = imread('test_images/barbara.tif');
+I = imread('test_images/lighthouse.tiff');
 [v,h,b] = size(I);
 B = bayer(I,v,h);
+figure(1)
 imshow(B)
+title('Lighthouse downsampled to Bayer CFA')
+
+%{
+figure(2)
+D = edi(B,v,h);
+imshow(D)
+title('Adaptive edge-directed interpolation')
+%}
