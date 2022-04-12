eb6ccc4ccb6e2bfcb71839376a83bb33b1d55654
Steven Hastings <borracha630@yahoo.com>
2022-04-12 12:08:24 -0400

    completed interpolation of R and B

--- edi.m
+++ edi.m
@@ -1,7 +1,8 @@
 function D = edi(B,v,h)
+  B = double(B);
   D = B;
   
-  % detect edge and interpolate G (minus border)
+  % detect edge and interpolate G
   for i = 2:v-1
     for j = 2:2:h-1
       
@@ -19,30 +20,45 @@ function D = edi(B,v,h)
       end
     end
   end
-  
-  % fill in border
-  
+ 
   % interpolate R at B
   for i = 3:2:v-1
     for j = 3:2:h-1
-      D(i,j,1) = B(i,j,2)*(B(i-1,j-1,1)/B(i-1,j-1,2)
-                         + B(i-1,j+1,1)/B(i-1,j+1,2)
-                         + B(i+1,j-1,1)/B(i+1,j-1,2)
-                         + B(i+1,j+1,1)/B(i+1,j+1,2))/4;
+      D(i,j,1) = D(i,j,2)*(D(i-1,j-1,1)/D(i-1,j-1,2)...
+                         + D(i-1,j+1,1)/D(i-1,j+1,2)...
+                         + D(i+1,j-1,1)/D(i+1,j-1,2)...
+                         + D(i+1,j+1,1)/D(i+1,j+1,2))/4;
     end
   end
   
   % interpolate B at R
   for i = 2:2:v-1
     for j = 2:2:h-1
-      D(i,j,3) = B(i,j,2)*(B(i-1,j-1,3)/B(i-1,j-1,2)
-                         + B(i-1,j+1,3)/B(i-1,j+1,2)
-                         + B(i+1,j-1,3)/B(i+1,j-1,2)
-                         + B(i+1,j+1,3)/B(i+1,j+1,2))/4;
+      D(i,j,3) = D(i,j,2)*(D(i-1,j-1,3)/D(i-1,j-1,2)...
+                         + D(i-1,j+1,3)/D(i-1,j+1,2)...
+                         + D(i+1,j-1,3)/D(i+1,j-1,2)...
+                         + D(i+1,j+1,3)/D(i+1,j+1,2))/4;
                         
     end
   end
   
-  % interpolate R,B at remaining G
-  
+  % interpolate R and B at remaining G
+  for i = 3:v-2
+    for j = 3:2:h-2
+
+        k = j + mod(i,2);
+
+        D(i,k,1) = D(i,k,2)*(D(i-1,k,1)/D(i-1,k,2)...
+                           + D(i+1,k,1)/D(i+1,k,2)...
+                           + D(i,k-1,1)/D(i,k-1,2)...
+                           + D(i,k+1,1)/D(i,k+1,2))/4;
+
+        D(i,k,3) = D(i,k,2)*(D(i-1,k,3)/D(i-1,k,2)...
+                           + D(i+1,k,3)/D(i+1,k,2)...
+                           + D(i,k-1,3)/D(i,k-1,2)...
+                           + D(i,k+1,3)/D(i,k+1,2))/4;
+    end
+  end
+
+  D = uint8(D);
 end
