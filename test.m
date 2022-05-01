aa4445ca071a0056459241f5ace26680fa661e3d
Steven Hastings <borracha630@yahoo.com>
2022-05-01 18:59:27 -0400

    demonstration of prefiltered image and bilinear interpolation

--- test.m
+++ test.m
@@ -1,17 +1,27 @@
 I = imread('test_images/lighthouse.tiff');
 
-subplot(1,3,1)
+subplot(1,5,1)
 imshow(I)
 title('Original')
 
 [v,h,b] = size(I);
+
+I = bayer_pref(I,v,h);
+subplot(1,5,2)
+imshow(I)
+title('Prefiltered image')
+
 B = bayer(I,v,h);
-subplot(1,3,2)
+subplot(1,5,3)
 imshow(B)
-title('Bayer CFA')
+title('Bayer-encoded image')
+
+L = bi(B);
+subplot(1,5,4)
+imshow(L)
+title('Bilinear interpolation')
 
 D = edi(B,v,h);
-subplot(1,3,3)
+subplot(1,5,5)
 imshow(D)
 title('Edge-directed interpolation')
-
